40716ad9a75fa4c4a7b955a557e12d135e4be9a1
Dominik Hahn <develop.illuminati@gmail.com>
2023-08-30 16:35:18 +0200

    refactor

--- src/main/java/eu/aboutdev/microservice/ApiGatewayApplication.java
+++ src/main/java/eu/aboutdev/microservice/ApiGatewayApplication.java
@@ -1,15 +1,10 @@
 package eu.aboutdev.microservice;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Metrics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import reactor.core.publisher.Mono;
-
-import java.util.Optional;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -21,15 +16,5 @@ public class ApiGatewayApplication {
     @Bean
     KeyResolver userKeyResolver() {
         return exchange -> Mono.just("1");
-    }
-
-    @Bean
-    @ConditionalOnClass(name = "io.opentelemetry.javaagent.OpenTelemetryAgent")
-    public MeterRegistry otelRegistry() {
-        Optional<MeterRegistry> otelRegistry = Metrics.globalRegistry.getRegistries().stream()
-                .filter(r -> r.getClass().getName().contains("OpenTelemetryMeterRegistry"))
-                .findAny();
-        otelRegistry.ifPresent(Metrics.globalRegistry::remove);
-        return otelRegistry.orElse(null);
     }
 }
