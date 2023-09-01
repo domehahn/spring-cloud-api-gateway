b71133a94528a12a714d80c258c161b7c14216ed
Dominik Hahn <develop.illuminati@gmail.com>
2023-09-01 21:42:13 +0200

    add ratelimit

---
+++ src/main/java/eu/aboutdev/microservice/ratelimiting/ApiKeyResolver.java
@@ -0,0 +1,13 @@
+package eu.aboutdev.microservice.ratelimiting;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class ApiKeyResolver implements KeyResolver {
+
+    @Override
+    public Mono<String> resolve(ServerWebExchange exchange) {
+        return Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("X-API-Key"));
+    }
+}
