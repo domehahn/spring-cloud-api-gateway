18e3649c4c0ef1aeee72850bb4bf46cb223f19fd
Dominik Hahn <develop.illuminati@gmail.com>
2023-09-01 22:37:18 +0200

    update config

--- src/main/java/eu/aboutdev/microservice/ratelimiting/ApiKeyResolver.java
+++ src/main/java/eu/aboutdev/microservice/ratelimiting/ApiKeyResolver.java
@@ -1,9 +1,11 @@
 package eu.aboutdev.microservice.ratelimiting;
 
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Component
 public class ApiKeyResolver implements KeyResolver {
 
     @Override
